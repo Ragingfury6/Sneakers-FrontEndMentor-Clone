d30b2a37a84f5099220fe281af761fa7a242370a
Ragingfury6 <trentsuperman@gmail.com>
2023-04-07 14:03:13 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    colors: {
+      'orange': 'hsl(26,100%,55%)',
+      'paleorange': 'hsl(25,100%,94%)',
+      'darkblue': 'hsl(220, 13%, 13%)',
+      'darkgrayblue': 'hsl(219, 9%, 45%)',
+      'grayblue': 'hsl(220, 14%, 75%)',
+      'lightgrayblue': 'hsl(223, 64%, 98%)',
+      'white': 'hsl(0,0%,100%)',
+      'black': 'hsl(0, 0%, 0%)',
+      'fadedblack': 'hsla(0, 0%, 0%,0.75)',
+    },
+    fontFamily:{
+      'body':['Kumbh Sans', 'sans-serif']
+    },
+    screens:{
+      'sm':'375px',
+      '2xl':'1000px'
+    }
+  },
+}
+
